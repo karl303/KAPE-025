a00b899429fb61c79f3abfac98acd4c8b050e7a8
Karl Erickson <karl303@gmail.com>
2021-09-18 15:06:27 -0400

    Added initial development of Arduino driver.

---
+++ KAPE-025_Driver.ino
@@ -0,0 +1,44 @@
+
+#define CS_OUT_PIN    13
+#define DATA_OUT_PIN  12
+#define CLK_OUT_PIN   11
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(CS_OUT_PIN, OUTPUT);
+  pinMode(DATA_OUT_PIN, OUTPUT);
+  pinMode(CLK_OUT_PIN, OUTPUT);
+}
+
+void loop() {
+  int k = 0;
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+  digitalWrite(CS_OUT_PIN, LOW);
+  digitalWrite(DATA_OUT_PIN, HIGH);
+  for(k = 0; k < 24; k++)
+  {
+    digitalWrite(CLK_OUT_PIN, LOW);
+    digitalWrite(CLK_OUT_PIN, HIGH);
+    digitalWrite(CLK_OUT_PIN, LOW);
+  }
+  digitalWrite(CS_OUT_PIN, HIGH);
+  
+  delay(1000);                       // wait for a second
+  
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
+  digitalWrite(CS_OUT_PIN, LOW);
+  digitalWrite(DATA_OUT_PIN, LOW);
+  for(k = 0; k < 24; k++)
+  {
+    digitalWrite(CLK_OUT_PIN, LOW);
+    digitalWrite(CLK_OUT_PIN, HIGH);
+    digitalWrite(CLK_OUT_PIN, LOW);
+  }
+  digitalWrite(CS_OUT_PIN, HIGH);
+  delay(1000);                       // wait for a second
+}
