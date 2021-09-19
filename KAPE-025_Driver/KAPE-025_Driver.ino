55d9cdcaad7c9738402515dc0544e265024e23c2
Karl <karl303@gmail.com>
2021-09-19 17:30:31 -0400

    Added code to set DACs to 0.4 and 2.0 V, to be shifted up to 0.8 and 4.0 V respectively.

--- KAPE-025_Driver/KAPE-025_Driver.ino
+++ KAPE-025_Driver/KAPE-025_Driver.ino
@@ -3,6 +3,8 @@
 #define DATA_OUT_PIN  12
 #define CLK_OUT_PIN   11
 
+int DacCode = 0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
@@ -13,10 +15,16 @@ void setup() {
 }
 
 void loop() {
+  // DAC code 31 corresponds to 0.401 Volts
+  //    31/255 * 3.3 = 0.401
+  // DAC code 155 corresponds to 2.005 Volts
+  //    155/255 * 3.3 = 2.005
+  
   int k = 0;
   // put your main code here, to run repeatedly:
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 
+  DacCode = 31;
   digitalWrite(CS_OUT_PIN, LOW);
   //digitalWrite(DATA_OUT_PIN, HIGH);
   for(k = 0; k < 24; k++)
@@ -24,6 +32,10 @@ void loop() {
     if(k == 1)
     {
       digitalWrite(DATA_OUT_PIN, HIGH);
+    }
+    else if(k > 15)
+    {
+      digitalWrite(DATA_OUT_PIN, (DacCode >> (23 - k)) & 0x1);
     }
     else
     {
@@ -35,18 +47,27 @@ void loop() {
   }
   digitalWrite(CS_OUT_PIN, HIGH);
   
-  delay(1000);                       // wait for a second
+  delay(2000);                       // wait for a second
   
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 
+  DacCode = 155;
   digitalWrite(CS_OUT_PIN, LOW);
   digitalWrite(DATA_OUT_PIN, LOW);
   for(k = 0; k < 24; k++)
   {
+    if(k > 15)
+    {
+      digitalWrite(DATA_OUT_PIN, (DacCode >> (23 - k)) & 0x1);
+    }
+    else
+    {
+      digitalWrite(DATA_OUT_PIN, LOW);
+    }
     digitalWrite(CLK_OUT_PIN, LOW);
     digitalWrite(CLK_OUT_PIN, HIGH);
     digitalWrite(CLK_OUT_PIN, LOW);
   }
   digitalWrite(CS_OUT_PIN, HIGH);
-  delay(1000);                       // wait for a second
+  delay(2000);                       // wait for a second
 }
